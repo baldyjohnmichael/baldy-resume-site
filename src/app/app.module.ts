10b9b962677dbec3d43d1549c86837cd1bb08eba
John-Michael Baldy <baldy.johnmichael@gmail.com>
2018-02-19 14:04:56 -0500

    init project and begin work on resume site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ProjectsComponent } from './projects/projects.component';
 
+const appRoutes: Routes = [
+  { path: '', component: ResumeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'projects', component: ProjectsComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ResumeComponent,
+    ProjectsComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+        { enableTracing: true } // <--- debugging purposes only
+      ),
     BrowserModule
   ],
   providers: [],
